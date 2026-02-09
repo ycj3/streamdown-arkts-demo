319ed3063f78df81fd3e93eaf0bc0db4fa2b5a77
CarlsonYuan <carlsonyuandev@gmail.com>
2026-02-09 12:41:09 +0800

    chore: rename streamdown as streamingmarkdown and chore: extract streaming-markdown as standalone library

--- streamdown/src/main/ets/core/reducer/reducers/index.ts
+++
@@ -1,8 +0,0 @@
-export { ParagraphReducer } from "./ParagraphReducer";
-export { HeadingReducer } from "./HeadingReducer";
-export { CodeFenceReducer } from "./CodeFenceReducer";
-export { InlineCodeReducer } from "./InlineCodeReducer";
-export { ListReducer } from "./ListReducer";
-export { OrderedListReducer } from "./OrderedListReducer";
-export { BlockquoteReducer } from "./BlockquoteReducer";
-export { HorizontalRuleReducer } from "./HorizontalRuleReducer";
